c43dce8c0f16f56f62f0f88de598a8d82b735b82
Marios Zindilis <marios@zindilis.com>
2025-01-14 09:39:09 +0000

    Added model Link Person-Photograph

---
+++ mneia_backend/models/links/person_photograph.py
@@ -0,0 +1,29 @@
+import rest_framework
+from django.contrib import admin
+from django.db import models
+
+from mneia_backend.models import abstract
+
+
+class LinkPersonPhotograph(abstract.LinkModel):
+    person = models.ForeignKey("Person", on_delete=models.PROTECT)
+    photograph = models.ForeignKey("Photograph", on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name_plural = "Links Person-Photograph"
+
+
+class LinkPersonPhotographSerializer(rest_framework.serializers.ModelSerializer):
+    class Meta:
+        model = LinkPersonPhotograph
+        fields = "__all__"
+
+
+class LinkPersonPhotographViewSet(rest_framework.viewsets.ModelViewSet):
+    queryset = LinkPersonPhotograph.objects.all()
+    serializer_class = LinkPersonPhotographSerializer
+
+
+@admin.register(LinkPersonPhotograph)
+class LinkPersonPhotographAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in LinkPersonPhotograph._meta.fields]
