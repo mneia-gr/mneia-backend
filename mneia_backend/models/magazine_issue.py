9931490a4e6a72c281808faf3ad3dff9750b80c4
Marios Zindilis <marios@zindilis.com>
2025-01-26 10:48:17 +0000

    Added initial export as YAML for MagazineIssue

--- mneia_backend/models/magazine_issue.py
+++ mneia_backend/models/magazine_issue.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+from babel.dates import format_date
 from django.contrib import admin
 from django.db import models
 
@@ -8,7 +11,7 @@ class MagazineIssue(abstract.Model):
     """
     Related schema: https://schema.org/PublicationIssue
 
-        * `magazine`, foreign key to Magazine
+    * `magazine`, foreign key to Magazine
     * `issue_number`: text, because some issues may have non-numeric numbering, e.g. Roman numerals
     * `order`: number, specifies the order in case the `issue_number` is not sortable, e.g. with Roman numerals
     * `date_published`, a date
@@ -22,8 +25,16 @@ class MagazineIssue(abstract.Model):
     def __str__(self) -> str:
         return f"{self.magazine.name} - Issue {self.issue_number}"
 
+    @property
+    def as_yaml(self) -> Dict:
+        return {
+            "issue_number": self.issue_number,
+            "date_published": format_date(self.date_published, format="long", locale="el_GR"),
+        }
+
     class Meta:
         verbose_name_plural = "Magazine Issues"
+        ordering = ["order"]
 
 
 @admin.register(MagazineIssue)
--- mneia_backend/tests/models/test_magazine_issue.py
+++ mneia_backend/tests/models/test_magazine_issue.py
@@ -7,3 +7,12 @@ from mneia_backend.models.magazine_issue import MagazineIssue
 def test_magazine_issue_str():
     magazine_issue = MagazineIssue.objects.get(id="77f9eb61-c3c3-4215-8d86-2dd522a1e4cc")  # from fixture
     assert str(magazine_issue) == "Μεγάλη Ελληνική Εγκυκλοπαίδεια - Issue 1"
+
+
+@pytest.mark.django_db
+def test_magazine_issue_as_yaml():
+    magazine = MagazineIssue.objects.get(id="77f9eb61-c3c3-4215-8d86-2dd522a1e4cc")  # from fixture
+    assert magazine.as_yaml == {
+        "issue_number": "1",
+        "date_published": "28 Φεβρουαρίου 1926",
+    }
